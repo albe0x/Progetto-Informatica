8713449df606419dd913fbbb883130b42f1fa06d
albe0x <buglialberto@gmail.com>
2026-05-05 18:34:57 +0200

    update

--- frontend/src/components/posts/Post.jsx
+++ frontend/src/components/posts/Post.jsx
@@ -1,7 +1,7 @@
 function Post({ post }) {
   return (
     /* Aumentato mb-12 a mb-20 per dare molto più spazio tra i post */
-    <div className="w-full block !mb-20"> 
+    <div className="w-full block !mb-40"> 
       <div className="
         w-full
         border-l-[6px] border-l-blue-500 
