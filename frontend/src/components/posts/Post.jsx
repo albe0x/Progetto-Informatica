884d5e10935734a27a6fd399660cb2933537d24f
albe0x <buglialberto@gmail.com>
2026-05-06 18:35:07 +0200

    update

--- frontend/src/components/posts/Post.jsx
+++ frontend/src/components/posts/Post.jsx
@@ -104,7 +104,7 @@ function Post({ post }) {
             <MessageSquare size={18} /> Commenti
           </button>
           <Link to={`/profile/${post.username}`} className="ml-auto text-xs font-black text-blue-500 uppercase tracking-widest">
-            @{post.username} {post.isSuperAdmin ? "ADMIN" : ""} {post.isVerified? "V": ""}
+            @{post.username} {post.isSuperAdmin ? "ADMIN" : "N"} {post.isVerified? "V": "N"}
           </Link>
         </div>
 
